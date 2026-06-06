4b55d6e293dd70451d02bf01cddc64a1755fbb21
Arnav <toarnavmahajan@gmail.com>
2026-06-06 17:13:52 +0530

    my learnin journey

---
+++ 16-Multithreading and Multiprocessing/advance_multi_processing.py
@@ -0,0 +1,18 @@
+### Multiproceesing with ProcessPoolExecutor
+
+from concurrent.futures import ProcessPoolExecutor
+import time
+
+def square_number(number):
+    time.sleep(1)
+    return f"Square: {number * number}"
+
+if __name__ == "__main__":
+
+    numbers = [1, 2, 3, 4, 5,6,7,8,9,2,3,5,6,7,8,9]
+
+    with ProcessPoolExecutor(max_workers=8) as executor:
+        results = list(executor.map(square_number, numbers))
+
+    for result in results:
+        print(result)
--- 16-Multithreading and Multiprocessing/advance_multi_threading.py
+++ 16-Multithreading and Multiprocessing/advance_multi_threading.py
@@ -7,7 +7,7 @@ def print_number(number):
     time.sleep(1)
     return f"Number: {number}"
 
-numbers = [1, 2, 3, 4, 5]
+numbers = [1, 2, 3, 4, 5,6,7,8,9,2,3,5,6,7,8,9]
 
 with ThreadPoolExecutor(max_workers=3) as executor:
     results = list(executor.map(print_number, numbers))
